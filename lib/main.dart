789996f65747e43d2ac4bb8b7a883ac320bb5741
peeweesusername <92876718+peeweesusername@users.noreply.github.com>
2023-02-27 11:59:20 -0500

    moved panels to separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scribblerremote/scribblers.dart';
+import 'package:scribblerremote/panels.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,8 +37,6 @@ class _ScribblerRemoteState extends State<ScribblerRemote> {
   bool _scribblerIsConnected = false;
   final List<Scribbler> _scribblers = [];
   late RCPanel myRCpanel;
-  //late ScannerPanel myScannerPanel;
-  //late StatefulScannerPanel mySFScannerPanel;
 
   void _doScan() {
     print('scanning');
@@ -58,7 +57,6 @@ class _ScribblerRemoteState extends State<ScribblerRemote> {
     print('called back - connected to scribbler');
     setState(() {
       _scribblerIsConnected = true;
-      //myScannerPanel.isConnected = true;
     });
   }
 
@@ -137,7 +135,7 @@ class _ScribblerRemoteState extends State<ScribblerRemote> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             //myScannerPanel,
-            StatefulScannerPanel(
+            ScannerPanel(
                 doScan: _doScan,
                 isConnected: _scribblerIsConnected
             ),
@@ -145,123 +143,6 @@ class _ScribblerRemoteState extends State<ScribblerRemote> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class StatefulScannerPanel extends StatefulWidget {
-  final VoidCallback doScan;
-  final bool isConnected;
-  const StatefulScannerPanel({
-    super.key,
-    required this.doScan,
-    required this.isConnected});
-
-  @override
-  State<StatefulScannerPanel> createState() => _StatefulScannerPanel();
-}
-
-class _StatefulScannerPanel extends State<StatefulScannerPanel> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        widget.isConnected ? const Text('connected') : const Text('not connected'),
-        FloatingActionButton(
-          onPressed: widget.doScan,
-          tooltip: 'Scan',
-          child: const Icon(Icons.search),
-        ),
-      ],);
-  }
-}
-/*
-class ScannerPanel extends StatelessWidget {
-  final VoidCallback doScan;
-  bool isConnected;
-
-  ScannerPanel(this.doScan, this.isConnected, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        isConnected ? const Text('connected') : const Text('not connected'),
-        FloatingActionButton(
-          onPressed: doScan,
-          tooltip: 'Scan',
-          child: const Icon(Icons.search),
-        ),
-      ],);
-  }
-}
-*/
-
-class RCPanel extends StatelessWidget {
-  final VoidCallback forward;
-  final VoidCallback left;
-  final VoidCallback right;
-  final VoidCallback stop;
-  final VoidCallback reverse;
-  final VoidCallback beep;
-  const RCPanel(
-      this.forward,
-      this.left,
-      this.right,
-      this.stop,
-      this.reverse,
-      this.beep,
-      {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FloatingActionButton(
-              onPressed: forward,
-              tooltip: 'Forward',
-              child: const Icon(Icons.arrow_upward),
-            ),
-          ]
-        ),
-        Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FloatingActionButton(
-                onPressed: left,
-                tooltip: 'Left',
-                child: const Icon(Icons.arrow_back),
-              ),
-              FloatingActionButton(
-                onPressed: stop,
-                tooltip: 'Stop',
-                child: const Icon(Icons.stop_circle),
-              ),
-              FloatingActionButton(
-                onPressed: right,
-                tooltip: 'Right',
-                child: const Icon(Icons.arrow_forward),
-              ),
-            ]
-        ),
-        FloatingActionButton(
-          onPressed: reverse,
-          tooltip: 'Reverse',
-          child: const Icon(Icons.arrow_downward),
-        ),
-        FloatingActionButton(
-          onPressed: beep,
-          tooltip: 'Beep',
-          child: const Icon(Icons.music_note),
-        ),
-      ],
     );
   }
 }
---
+++ lib/panels.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:scribblerremote/scribblers.dart';
+
+class StatefulScannerPanel extends StatefulWidget {
+  final VoidCallback doScan;
+  final bool isConnected;
+  const StatefulScannerPanel({
+    super.key,
+    required this.doScan,
+    required this.isConnected});
+
+  @override
+  State<StatefulScannerPanel> createState() => _StatefulScannerPanel();
+}
+
+class _StatefulScannerPanel extends State<StatefulScannerPanel> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        widget.isConnected ? const Text('connected') : const Text('not connected'),
+        FloatingActionButton(
+          onPressed: widget.doScan,
+          tooltip: 'Scan',
+          child: const Icon(Icons.search),
+        ),
+      ],);
+  }
+}
+
+class ScannerPanel extends StatelessWidget {
+  final VoidCallback doScan;
+  final bool isConnected;
+
+  const ScannerPanel({
+    super.key,
+    required this.doScan,
+    required this.isConnected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        isConnected ? const Text('connected') : const Text('not connected'),
+        FloatingActionButton(
+          onPressed: doScan,
+          tooltip: 'Scan',
+          child: const Icon(Icons.search),
+        ),
+      ],);
+  }
+}
+
+class RCPanel extends StatelessWidget {
+  final VoidCallback forward;
+  final VoidCallback left;
+  final VoidCallback right;
+  final VoidCallback stop;
+  final VoidCallback reverse;
+  final VoidCallback beep;
+  const RCPanel(
+      this.forward,
+      this.left,
+      this.right,
+      this.stop,
+      this.reverse,
+      this.beep,
+      {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FloatingActionButton(
+                onPressed: forward,
+                tooltip: 'Forward',
+                child: const Icon(Icons.arrow_upward),
+              ),
+            ]
+        ),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FloatingActionButton(
+                onPressed: left,
+                tooltip: 'Left',
+                child: const Icon(Icons.arrow_back),
+              ),
+              FloatingActionButton(
+                onPressed: stop,
+                tooltip: 'Stop',
+                child: const Icon(Icons.stop_circle),
+              ),
+              FloatingActionButton(
+                onPressed: right,
+                tooltip: 'Right',
+                child: const Icon(Icons.arrow_forward),
+              ),
+            ]
+        ),
+        FloatingActionButton(
+          onPressed: reverse,
+          tooltip: 'Reverse',
+          child: const Icon(Icons.arrow_downward),
+        ),
+        FloatingActionButton(
+          onPressed: beep,
+          tooltip: 'Beep',
+          child: const Icon(Icons.music_note),
+        ),
+      ],
+    );
+  }
+}
